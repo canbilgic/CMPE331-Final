755ca131b2082a69e6910f067188457e969f6741
emresonmez02 <emre.sonmez02@gmail.com>
2016-12-20 09:59:43 +0200

    Update AccountHome.java
    imported librarys and creating Main class

--- AccountHome.java
+++ AccountHome.java
@@ -1,1 +1,48 @@
+//AccountHome.java is main page of project. which included profile, new reservation, cancel reservation and search bus.
 
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
+import java.awt.Label;
+import java.awt.SystemColor;
+import java.sql.Date;
+import java.util.Calendar;
+import java.awt.Font;
+import java.awt.Canvas;
+import javax.swing.Box;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+
+public class AccountHome extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AccountHome frame = new AccountHome();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
