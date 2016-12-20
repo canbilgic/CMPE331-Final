c85d2d03426292b8e3523881753ad0ad4cbb3e14
canbilgic <can.bilgic@bilgiedu.net>
2016-12-20 09:52:01 +0200

    Create Home.java
    imported libraries and only main function.

---
+++ Home.java
@@ -0,0 +1,39 @@
+// Home.java is include our homepage, register, login and search functions. 
+
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Label;
+import java.awt.Canvas;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Home extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Home frame = new Home();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
