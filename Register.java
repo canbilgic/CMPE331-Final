1838c51e52b9b73cbad2793f470313095dca81ee
emresonmez02 <emre.sonmez02@gmail.com>
2016-12-20 10:19:50 +0200

    Update Register.java
    imported library

--- Register.java
+++ Register.java
@@ -1,1 +1,21 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Label;
+import java.awt.SystemColor;
+import java.awt.Font;
+import java.awt.Canvas;
+import java.awt.TextField;
+import java.awt.ComponentOrientation;
+import java.awt.Button;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
+import javax.swing.JFormattedTextField;
